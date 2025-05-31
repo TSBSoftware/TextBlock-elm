0101776927c2d7b4141089fdb63c50085e342591
Jeroen Engels <jfm.engels@gmail.com>
2025-05-31 22:42:08 +0200

    Move let values inside the test

--- tests/TextBlockTests.elm
+++ tests/TextBlockTests.elm
@@ -7,101 +7,101 @@ import TextBlock exposing (..)
 
 emptyValueTests : Test
 emptyValueTests =
-    let
-        testValue : String
-        testValue =
-            """
-            """
-    in
     test "Can read empty multiline string return empty string" <|
         \() ->
+            let
+                testValue : String
+                testValue =
+                    """
+            """
+            in
             textBlock testValue
                 |> Expect.equal ""
 
 
 simpleValueTests : Test
 simpleValueTests =
-    let
-        testValue : String
-        testValue =
-            """Hello"""
-    in
     test "Can read simple value return unchanged value" <|
         \() ->
+            let
+                testValue : String
+                testValue =
+                    """Hello"""
+            in
             textBlock testValue
                 |> Expect.equal "Hello"
 
 
 simpleValueTwoLineTests : Test
 simpleValueTwoLineTests =
-    let
-        testValue : String
-        testValue =
-            """
-            Hello"""
-    in
     test "Can read simple value on two lines and return single value" <|
         \() ->
+            let
+                testValue : String
+                testValue =
+                    """
+                    Hello"""
+            in
             textBlock testValue
                 |> Expect.equal "Hello"
 
 
 oneLineValueTests : Test
 oneLineValueTests =
-    let
-        testValue : String
-        testValue =
-            """
-            Hello, World!
-            """
-
-        expected : String
-        expected =
-            "Hello, World!"
-    in
     test "One line string gets returned with no padding" <|
         \() ->
+            let
+                testValue : String
+                testValue =
+                    """
+                    Hello, World!
+                    """
+
+                expected : String
+                expected =
+                    "Hello, World!"
+            in
             textBlock testValue
                 |> Expect.equal expected
 
 
 twoLineValueTests : Test
 twoLineValueTests =
-    let
-        testValue : String
-        testValue =
-            """
-            Hello, World!
-            I am me.
-            """
-
-        expected : String
-        expected =
-            "Hello, World!\nI am me."
-    in
     test "Two line string gets returned with no padding" <|
         \() ->
+            let
+                testValue : String
+                testValue =
+                    """
+                    Hello, World!
+                    I am me.
+                    """
+
+                expected : String
+                expected =
+                    "Hello, World!\nI am me."
+            in
             textBlock testValue
                 |> Expect.equal expected
 
 
 newLineEscapeTests : Test
 newLineEscapeTests =
-    let
-        testValue : String
-        testValue =
-            "\n"
-                ++ "    Hello, World! \\\n"
-                ++ "    I am me. \\\n"
-                ++ "    Who are you?\n"
-                ++ "    "
-
-        expected : String
-        expected =
-            "Hello, World! I am me. Who are you?"
-    in
     test "Newlines omitted when \\ is used at end of line" <|
         \() ->
+            let
+                testValue : String
+                testValue =
+                    "\n"
+                        ++ "    Hello, World! \\\n"
+                        ++ "    I am me. \\\n"
+                        ++ "    Who are you?\n"
+                        ++ "    "
+
+                expected : String
+                expected =
+                    "Hello, World! I am me. Who are you?"
+            in
             textBlock testValue
                 |> Expect.equal expected
 
@@ -180,20 +180,20 @@ indentedContentTests =
 
 trailingSpaceTests : Test
 trailingSpaceTests =
-    let
-        testValue : String
-        testValue =
-            """
-            This text must    |
-            be blocked        |
-            """
-
-        expected : String
-        expected =
-            "This text must    \nbe blocked        "
-    in
     test "Lines ending in | keep trailing spaces" <|
         \() ->
+            let
+                testValue : String
+                testValue =
+                    """
+                    This text must    |
+                    be blocked        |
+                    """
+
+                expected : String
+                expected =
+                    "This text must    \nbe blocked        "
+            in
             textBlock testValue
                 |> Expect.equal expected
 
@@ -278,45 +278,46 @@ GH
 
 embededNewlineTests : Test
 embededNewlineTests =
-    let
-        testValue : String
-        testValue =
-            """
-            Name | Address | City
-            Bob Smith | 123 Anytown St
-Apt 100 | Vancouver
-            Jon Brown | 1000 Golden Place
-Suite 5 | Santa Ana
-            """
-
-        expected : String
-        expected =
-            "Name | Address | City\n"
-                ++ "Bob Smith | 123 Anytown St\n"
-                ++ "Apt 100 | Vancouver\n"
-                ++ "Jon Brown | 1000 Golden Place\n"
-                ++ "Suite 5 | Santa Ana"
-    in
     test "Embedded newline characters create new lines" <|
         \() ->
+            let
+                testValue : String
+                testValue =
+                    """
+                Name | Address | City
+                Bob Smith | 123 Anytown St
+Apt 100 | Vancouver
+                Jon Brown | 1000 Golden Place
+Suite 5 | Santa Ana
+                """
+
+                expected : String
+                expected =
+                    "Name | Address | City\n"
+                        ++ "Bob Smith | 123 Anytown St\n"
+                        ++ "Apt 100 | Vancouver\n"
+                        ++ "Jon Brown | 1000 Golden Place\n"
+                        ++ "Suite 5 | Santa Ana"
+            in
             textBlock testValue
                 |> Expect.equal expected
 
 
 formatStringTests : Test
 formatStringTests =
-    let
-        testValue =
-            """
-            Hello {{name}},
-
-            Goodbye
-            """
-
-        expected =
-            "Hello Chris,\n\nGoodbye"
-    in
     test "Can be parsed and replace template values" <|
         \() ->
+            let
+                testValue : String
+                testValue =
+                    """
+                    Hello {{name}},
+
+                    Goodbye
+                    """
+
+                expected =
+                    "Hello Chris,\n\nGoodbye"
+            in
             textBlockWithFormat defaultOptions [ ( "name", "Chris" ) ] testValue
                 |> Expect.equal expected
